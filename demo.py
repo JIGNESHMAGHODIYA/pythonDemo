70d5de18e4cd3073d7973e1a97d96b9ddb9231cd
SachinRajyaguru <41114520+SachinRajyaguru@users.noreply.github.com>
2019-05-09 20:22:20 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,27 @@
+class PrintDemo:
+
+    def __init__(self, prt):
+        self.strs = prt
+
+    def show(self):
+        print(self.strs)
+
+    def update(self,i):
+        self.strs += ' ' + str(i)
+
+
+
+class main:
+    objs = []
+    names = ['jay','sachin','yash','paresh','dilip','vimal','mukesh','mahesh','vijay','ravi']
+
+    for i in names:
+        objs.append(PrintDemo(i))
+
+    for num,i in enumerate(objs) :
+        objs[num].show()
+        objs[num].update(num)
+        objs[num].show()
+        print()
+
+
